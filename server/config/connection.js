648dac7ebfa429d339ce99715d0883cc91eb8e82
Bradley Troll <bradleytroll@gmail.com>
2024-06-06 20:59:44 -0400

    Add Heroku build configs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,14 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/beanbanter');
+
+// module.exports = mongoose.connection;
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/beanbanter');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/beanbanter', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
